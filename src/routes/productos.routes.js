1c0661f3846fcdbc4b68617dac7e299c1524f65b
dht04 <dht04@LENOVO_DHT>
2026-03-25 11:26:11 -0600

    Oviedo3

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { crearProducto, actualizarProducto,obtenerProductos, obtenerProductoPorId } from '../controllers/productos.controller.js';
+import { crearProducto, actualizarProducto,obtenerProductos, obtenerProductoPorId, eliminarProducto } from '../controllers/productos.controller.js';
 import { verificarToken } from '../middlewares/auth.middleware.js';
 import { upload } from '../middlewares/upload.js';
 
@@ -11,4 +11,5 @@ router.get('/:id', obtenerProductoPorId);
 // Rutas protegidas
 router.post('/', upload.single('imagen'), crearProducto);
 router.put('/:id', upload.single('imagen'), actualizarProducto);
+routerd.delete('/:id', eliminarProducto);
 export default router;
