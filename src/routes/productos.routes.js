4d0170710e980bc474ead6c80b04fb89a5fa64f2
dht04 <dht04@LENOVO_DHT>
2026-03-25 11:17:55 -0600

    Oviedo2

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { crearProducto, actualizarProducto,obtenerProductos } from '../controllers/productos.controller.js';
+import { crearProducto, actualizarProducto,obtenerProductos, obtenerProductoPorId } from '../controllers/productos.controller.js';
 import { verificarToken } from '../middlewares/auth.middleware.js';
 import { upload } from '../middlewares/upload.js';
 
@@ -7,7 +7,7 @@ const router = Router();
 
 // Rutas públicas
 router.get('/', obtenerProductos);
-
+router.get('/:id', obtenerProductoPorId); 
 // Rutas protegidas
 router.post('/', upload.single('imagen'), crearProducto);
 router.put('/:id', upload.single('imagen'), actualizarProducto);
