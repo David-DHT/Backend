bb3614407e6970d9314d270b7a4bbccf862907ad
dht04 <dht04@LENOVO_DHT>
2026-04-11 22:18:10 -0600

    4

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -82,12 +82,12 @@ export const login = async (req, res) => {
 
 
         //payload con id y perfil para el token
-        const payload = {id: usuario.id,idPerfil: usuario.idPerfil };
+        const payload = {id: usuario.idUsuario,idPerfil: usuario.idPerfil };
 
         const token = jwt.sign(payload, process.env.JWT_SECRET, {
         expiresIn: '1h' 
         });
-        res.status(200).json({ message: 'Inicio de sesión exitoso', token, idPerfil: usuario.idPerfil,idUsuario: usuario.id });
+        res.status(200).json({ message: 'Inicio de sesión exitoso', token, idPerfil: usuario.idPerfil, id: usuario.idUsuario });
 
     }
     catch (error) {
